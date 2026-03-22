a75019ce665a201ed6294004c10631592e4495e9
Uday Senapati <udaysenapati6878@gmail.com>
2026-03-23 02:01:27 +0530

    feat(app): register menu and qr modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { QrModule } from './modules/qr-management/qr.module';
+import { PrismaModule } from './modules/prisma/prisma.module';
+import { MenuModule } from './modules/menu/menu.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    QrModule,
+    PrismaModule,
+    MenuModule
+  ],
 })
 export class AppModule {}
