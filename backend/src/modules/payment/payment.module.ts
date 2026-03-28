ef18174744263567a9aa18f30a951bf5105d2ee3
Uday Senapati <udaysenapati6878@gmail.com>
2026-03-29 01:25:50 +0530

    update: payment module to fix redis connection error

--- backend/src/modules/payment/payment.module.ts
+++ backend/src/modules/payment/payment.module.ts
@@ -1,12 +1,27 @@
 import { Module } from '@nestjs/common';
 import { BullModule } from '@nestjs/bullmq';
+import IORedis from 'ioredis';
+
 import { PaymentController } from './payment.controller';
 import { PaymentService } from './payment.service';
 import { PrismaService } from '../prisma/prisma.service';
 import { ORDER_QUEUE } from '../order/order.processor';
 
+const redisUrl = process.env.REDIS_PUBLIC_URL;
+
+if (!redisUrl) {
+  throw new Error('REDIS_PUBLIC_URL not set');
+}
+
 @Module({
-  imports: [BullModule.registerQueue({ name: ORDER_QUEUE })],
+  imports: [
+    BullModule.registerQueue({
+      name: ORDER_QUEUE,
+      connection: new IORedis(redisUrl, {
+        maxRetriesPerRequest: null,
+      }),
+    }),
+  ],
   controllers: [PaymentController],
   providers: [PaymentService, PrismaService],
 })
