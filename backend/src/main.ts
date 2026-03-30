95fc910b68ad8fff5d1ac10b61eede11c7c6a84f
Uday Senapati <udaysenapati6878@gmail.com>
2026-03-30 15:33:57 +0530

    backend: fix dotenv issue

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,10 @@
+import 'reflect-metadata';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import 'reflect-metadata';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
